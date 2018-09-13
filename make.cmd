24293e1e74d5a849e2b222b9258572a1dba3827f
prokushev <yuri.prokushev@gmail.com>
2018-09-13 15:45:51 +0700

    More tools to build was added. Startup code downgraded to 8080 CPU.

--- make.cmd
+++ make.cmd
@@ -239,6 +239,55 @@ zmac --rel --zmac strlib\token.z80
 zmac --rel --zmac strlib\tokeng.z80
 zmac --rel --zmac strlib\tokenl.z80
 rem Build CFCB
+zmac --rel --zmac cfcb\bytelen.z80
+zmac --rel --zmac cfcb\bytelen2.z80
+zmac --rel --zmac cfcb\cbyti.z80
+zmac --rel --zmac cfcb\cbyto.z80
+zmac --rel --zmac cfcb\ccblen.z80
+zmac --rel --zmac cfcb\ccdev.z80
+zmac --rel --zmac cfcb\ccdma.z80
+zmac --rel --zmac cfcb\ccfixf.z80
+zmac --rel --zmac cfcb\ccgdrv.z80
+zmac --rel --zmac cfcb\ccget.z80
+zmac --rel --zmac cfcb\ccgusr.z80
+zmac --rel --zmac cfcb\ccirec.z80
+zmac --rel --zmac cfcb\ccirrc.z80
+zmac --rel --zmac cfcb\cclast.z80
+zmac --rel --zmac cfcb\cclose.z80
+zmac --rel --zmac cfcb\ccpw.z80
+zmac --rel --zmac cfcb\ccrrec.z80
+zmac --rel --zmac cfcb\ccrrrc.z80
+zmac --rel --zmac cfcb\ccwrec.z80
+zmac --rel --zmac cfcb\ccxtnd.z80
+zmac --rel --zmac cfcb\cczfcb.z80
+zmac --rel --zmac cfcb\cdel.z80
+zmac --rel --zmac cfcb\cdevio.z80
+zmac --rel --zmac cfcb\cdvini.z80
+zmac --rel --zmac cfcb\cdvtest.z80
+zmac --rel --zmac cfcb\cerror.z80
+zmac --rel --zmac cfcb\cexwild.z80
+zmac --rel --zmac cfcb\cfdos.z80
+zmac --rel --zmac cfcb\cfind.z80
+zmac --rel --zmac cfcb\cfitest.z80
+zmac --rel --zmac cfcb\cgeof.z80
+zmac --rel --zmac cfcb\cifdev.z80
+zmac --rel --zmac cfcb\cifnul.z80
+zmac --rel --zmac cfcb\cmake.z80
+zmac --rel --zmac cfcb\cmkdev.z80
+zmac --rel --zmac cfcb\copen.z80
+zmac --rel --zmac cfcb\copfcb.z80
+zmac --rel --zmac cfcb\cparse.z80
+zmac --rel --zmac cfcb\cpwset.z80
+zmac --rel --zmac cfcb\crbyti.z80
+zmac --rel --zmac cfcb\crclose.z80
+zmac --rel --zmac cfcb\crdtst.z80
+zmac --rel --zmac cfcb\cread.z80
+zmac --rel --zmac cfcb\cseek.z80
+zmac --rel --zmac cfcb\cseof.z80
+zmac --rel --zmac cfcb\ctell.z80
+zmac --rel --zmac cfcb\ctmake.z80
+zmac --rel --zmac cfcb\cwrite.z80
+zmac --rel --zmac cfcb\tdos.z80
 rem Build NETLIB
 zmac --rel --zmac netlib\nacsdrv.z80
 zmac --rel --zmac netlib\ncfgtbl.z80
@@ -255,9 +304,49 @@ zmac --rel --zmac netlib\nscfgtbl.z80
 zmac --rel --zmac netlib\nsendmsg.z80
 zmac --rel --zmac netlib\nulckrec.z80
 
+zmac --rel --zmac last.z80
+
+rem Build ENDLIST
 zmac --rel --zmac nendlist.z80
-ld80 -o nendlist.hex -P100 -m -s nendlist.sym nendlist.rel SLOUT.REL SBDOS.REL
-load <nendlist.hex >nendlist.com
-ld80 -o nlogoff.hex -P100 -m -s nlogoff.sym nlogoff.rel SPRINTT.REL SBDOS.REL
-load <nlogoff.hex >nlogoff.com
+ld80 -o nendlist.hex -P100 -m -s nendlist.sym zout\nendlist.rel zout\slout.rel zout\sbdos.rel
+load <nendlist.hex >bin\endlist.com
+
+rem Build LOGOFF
+zmac --rel --zmac nlogoffc.z80
+ld80 -o nlogoffc.hex -P100 -m -s nlogoff.sym zout\nlogoffc.rel zout\sprint.rel zout\spstr.rel zout\scout.rel zout\sccout.rel zout\sbdos.rel zout\nlogoff.rel zout\ncfgtbl.rel
+load <nlogoffc.hex >bin\logoff.com
+
+rem Build 3TYPE
+zmac --rel --zmac 3type.z80
+ld80 -o 3type.hex -P100 -m -s 3type.sym zout\3type.rel zout\last.rel zout\sbdos.rel zout\sprint.rel zout\spstr.rel zout\scout.rel zout\sccout.rel zout\copen.rel zout\c3page.rel zout\c3passwd.rel zout\c3push.rel zout\c3opt.rel zout\c3err.rel zout\c3scbget.rel zout\c3brk.rel zout\cerror.rel zout\cfdos.rel zout\sbout.rel zout\scin.rel zout\ccdma.rel zout\ccpw.rel zout\ccgusr.rel zout\ccget.rel zout\copfcb.rel zout\crbyti.rel zout\scaps.rel zout\scrlf.rel zout\cifnul.rel zout\cifdev.rel zout\ccblen.rel zout\cdevio.rel zout\ccdev.rel zout\ccgdrv.rel zout\ccfixf.rel zout\ccrrrc.rel zout\ccirrc.rel zout\cczfcb.rel zout\cclast.rel zout\cexwild.rel zout\cpwset.rel zout\cparse.rel zout\crclose.rel
+load <3type.hex >bin\type.com
+
+rem Build 3DATE
+zmac --rel --zmac 3date.z80
+ld80 -o 3date.hex -P100 -m -s 3date.sym zout\3date.rel zout\sbdos.rel zout\sprint.rel zout\spstr.rel zout\scout.rel zout\sccout.rel zout\scrlf.rel zout\sbout.rel zout\sbline.rel zout\c3err.rel zout\scaps.rel zout\scondin.rel zout\scin.rel zout\scst.rel zout\c3a2date.rel zout\c3a2time.rel zout\c3date2a.rel zout\c3time2a.rel zout\seval1.rel zout\sma2hc.rel zout\smafdc.rel zout\smhlfdc.rel zout\sen.rel zout\c3dname.rel zout\c3gtime.rel zout\c3stime.rel zout\c3day.rel zout\c3dfmt.rel zout\c3dmy.rel zout\c3tbcd.rel zout\smth12.rel zout\smth11.rel zout\c3int.rel zout\c3push.rel zout\c3scbget.rel
+load <3date.hex >bin\date.com
+
+rem Build 2GO
+zmac --zmac 2go.z80
+load <zout\2go.hex >bin\go.com
+
+rem Build 2STAT
+zmac -8 --zmac 2stat.z80
+: почему-то виснет...
+:load <zout\2stat.hex >bin\stat.com
+
+rem Build 2SUBMIT
+zmac -8 --zmac 2submit.z80
+load <zout\2submit.hex >bin\submit.com
+
+rem Build 2CCP
+zmac -8 2ccp.z80
+: наверное, надо в BIN
+:load <zout\2ccp.hex >bin\ccp.com
+
+rem Build 3device
+zmac --rel --zmac 3device.z80
+ld80 -o 3device.hex -P100 -m -s 3device.sym zout\3device.rel zout\sbdos.rel zout\sprint.rel zout\spstr.rel zout\scout.rel zout\sccout.rel
+load <zout\3device.hex >bin\device.com
+
 exit
