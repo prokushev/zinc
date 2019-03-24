f337a42dd9ebda86295404ff2df76f0d5632f7c5
prokushev <yuri.prokushev@gmail.com>
2019-03-24 22:02:36 +0700

    sync

--- xbios/charaddr.asm
+++ xbios/charaddr.asm
@@ -1,19 +1,22 @@
+Более быстрый и короткий алгоритм вычисления начала символа.
+Поддерживаются символы с кодами до 7F
+
 Вычисление адреса буквы в знакогенератора по адресу 3c00
    Было:             Стало:
      LD L,A            LD L,A
      LD H,0            ADD HL,HL
-     ADD HL,HL         LD H,15
+     ADD HL,HL         LD H,15h
      ADD HL,HL         ADD HL,HL
      ADD HL,HL         ADD HL,HL
-     LD BC,3c00
+     LD BC,3c00h
      ADD HL,BC
 
 Вычисление адреса буквы в знакогенератора по адресу 3000
    Было:             Стало:
      LD L,A            LD L,A
      LD H,0            ADD HL,HL
-     ADD HL,HL         LD H,12
+     ADD HL,HL         LD H,12h
      ADD HL,HL         ADD HL,HL
      ADD HL,HL         ADD HL,HL
-     LD BC,3000
+     LD BC,3000h
      ADD HL,BC
